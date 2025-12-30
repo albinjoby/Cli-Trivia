ce0091f2a753321334fe9ea8fe10e4042c4dbdb1
albinjoby <albinjobyponnu@gmail.com>
2025-12-30 11:40:35 +0530

    add cli logic

--- index.ts
+++ index.ts
@@ -5,6 +5,13 @@ import color from 'picocolors'
 import {setTimeout} from 'timers/promises'
 import shuffle from 'lodash.shuffle'
 
+let SCORE = 0
+
+interface option{
+  value:string,
+  label:string
+}
+
 class Question{
   question: string;
   answersArray: string[];
@@ -19,7 +26,7 @@ class Question{
 
 const questions = async(): Promise<Question[]> => {
   const result = await fetch("https://opentdb.com/api.php?amount=5&category=18&type=multiple")
-  const data = await result.json()
+  const data:any = await result.json()
   
   return data.results.map((q: any) => {
     const answerArray = shuffle([...q.incorrect_answers, q.correct_answer])
@@ -27,4 +34,37 @@ const questions = async(): Promise<Question[]> => {
   })
 }
 
-console.log(await questions())
+async function askQuestion(question:string, answerArray:string[], correctAnswer:string){
+  const options:option[] = []
+  answerArray.forEach((answer)=>{
+    options.push({value:answer, lable:answer})
+  })
+  const answer = await p.select({
+    message: question,
+    options: options,
+    initialValue: '1'
+  })
+  
+  const s = p.spinner()
+  s.start()
+  await setTimeout(500)
+  s.stop()
+  
+  if (answer === correctAnswer){
+    SCORE++
+  }
+}
+
+async function main(){
+  p.intro('hello')
+  
+  const questionsList = await questions()
+  for (const question of questionsList){
+    await askQuestion(question.question, question.answersArray, question.correctAnswer)
+  }
+  
+  p.note(`your score is ${SCORE}`)
+  p.outro('bye')
+}
+
+main()
